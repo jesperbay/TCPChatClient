178b34507b4dba684cff34a577eaf4faeba44255
Jesper Nørager Bay <jesp8115@edu.easj.dk>
2017-09-15 10:38:05 +0200

    test server

--- TCPChatClient/Program.cs
+++ TCPChatClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,9 @@ namespace TCPChatClient
     {
         static void Main(string[] args)
         {
+            TcpClient connctionSocket = new TcpClient("localhost", 6789);
+            Console.WriteLine("Clint started");
+
         }
     }
 }
