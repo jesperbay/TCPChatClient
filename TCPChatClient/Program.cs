bd70531d041fb4e85c0e94c6a9275d4f08c09ae0
Jesper Nørager Bay <jesp8115@edu.easj.dk>
2017-09-15 11:27:50 +0200

    add stop and while

--- TCPChatClient/Program.cs
+++ TCPChatClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,15 +13,37 @@ namespace TCPChatClient
     {
         static void Main(string[] args)
         {
+            
+
             TcpClient clientSocket = new TcpClient("localhost", 6789);
             Console.WriteLine("Clint started");
-
             Stream ns = clientSocket.GetStream();
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true;
+            
 
-            string memessage = Console.ReadLine();
-            sw.WriteLine(memessage);
+            while (true)
+            {
+                StreamReader sr = new StreamReader(ns);
+                string message = sr.ReadLine();
+                Console.WriteLine("clit " + message);
+
+
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+
+                string memessage = Console.ReadLine();
+                sw.WriteLine(memessage);
+
+                string s = Console.ReadLine();
+                if (s == "end")
+                {
+                    sr.Close();
+                    sw.Close();
+                    ns.Close();
+                }
+
+            }
+
+           
 
         }
     }
